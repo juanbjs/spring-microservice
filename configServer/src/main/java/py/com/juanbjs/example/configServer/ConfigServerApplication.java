8fe1fdf50769143705f27ca3fad765ed36a77294
Juan Zarate <chitozarate92@gmail.com>
2019-11-14 22:48:09 -0300

    Dockerized config_server and Eureka_server

--- configServer/src/main/java/py/com/juanbjs/example/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/py/com/juanbjs/example/configServer/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package py.com.juanbjs.example.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
